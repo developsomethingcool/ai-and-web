27e2408b2d9c77618ab75002b4c0e13793c89eb3
enya <wiegmann.enya@gmail.com>
2024-12-26 20:50:21 +0100

    Your commit message here

---
+++ crawl.py
@@ -0,0 +1,15 @@
+# used to crawl the website
+# starting URL and follow links
+
+
+# import libraries
+import requests
+from bs4 import BeautifulSoup
+# what is this for: import re??
+
+# fetching the pagey by requests
+
+# aprsing bs
+
+# storing links
+
