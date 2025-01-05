584bfe1bfe4de2b129c4988dbd4f9336d3eabf37
Oleks <pometunalexander@gmail.com>
2025-01-05 18:14:33 +0100

    imporved interface
    Added search again and check of the input

--- Project2/flask_app.py
+++ Project2/flask_app.py
@@ -14,49 +14,35 @@ def get_crawler():
 def start():
     # Form to enter search terms and optional base URL
     return """
+        <h1>Welcome to the Search App</h1>
+        <p>Enter a term below to search the web index:</p>
         <form action='word_search' method='get'>
-            <input name='rev' placeholder='Enter search term'></input>
+            <input name='q' placeholder='Enter search term'></input>
             <button type='submit'>Search</button>
         </form>
     """
-
-# @app.route("/")
-# def start():
-#     return "<form action='word_search' method='get'><input name='rev'></input></form>"
 
 @app.route("/word_search")
 def word_search():
     crawler = get_crawler()
 
     # Get the search term from the query string
-    search_term = request.args.get('rev', '')  # Default to empty string if 'rev' is not provided
+    search_term = request.args.get('q', '') 
     
     # Perform the search
     search_results = crawler.search([search_term])
 
-    # Format results as clickable links
     if not search_results:
-        return f"<h1>No results found for '{search_term}'</h1>"
+        return f"""
+            <h1>No results found for '{search_term}'</h1>
+            <a href='/'>Search again</a>
+        """
 
     results_html = "".join(f"<li><a href='{url}'>{url}</a></li>" for url in search_results)
-    return f"<h1>Search results for '{search_term}':</h1><ul>{results_html}</ul>"
+    return f"""
+        <h1>Search results for '{search_term}':</h1>
+        <ul>{results_html}</ul>
+        <a href='/'>Search again</a>
+    """
 
 
-# @app.route("/word_search")
-# def word_search():
-#     base_url = "https://vm009.rz.uos.de/crawl/index.html"
-#     crawler = WebCrawler(base_url)
-#     crawler.crawl(base_url)
-
-#     # Get the search term from the query string
-#     search_term = request.args.get('rev', '')  # Default to empty string if 'rev' is not provided
-    
-#     # Perform the search
-#     search_results = crawler.search([search_term])
-
-#     results_html = "".join(f"<li><a href='{url}'>{url}</a></li>" for url in search_results)
-#     return f"<h1>Search results for '{search_term}':</h1><ul>{results_html}</ul>"
-
-
-    # Return the results as an HTML response
-    #return f"<h1>Search results for '{search_term}':</h1><p>{search_results}</p>"
